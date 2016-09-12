d10ddfb9858eb49b9c1bf9c24c11278e2704e873
mikhail <mandarson@gmail.com>
2016-09-12 20:30:02 +0300

    working but really buggy

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import re
 import MySQLdb
 import os
 import fnmatch
-import itertools
 import time
 from docx import Document
 import sys
@@ -20,24 +19,32 @@ class FirstParseErrorException(Exception):
 class SecondParseErrorException(Exception):
     pass
 
+def my_insert(arr, index, el):
+    ans = list(arr)
+    ans.insert(index, el)
+    return ans
+
 def scan_and_send_new_tickets(time_float, file_path):
     doc_creation_date = os.path.getmtime(file_path)
     if doc_creation_date < time_float:
         return None
-    #parse_docx(file_path)
+    parse_docx_and_send_to_db(file_path)
 
 def iterator(path):
     for dirpath, dirnames, files in os.walk(path):
         for f in fnmatch.filter(files, '*.docx'):
             yield '/'.join([dirpath,f])
-#format = ['ip', 'string', 'ip', 'string']
+
+
 FORMAT_TO_REG_MATCH = {
     'ip': r'\d+\.\d+\.\d+\.\d+',
-    'string': r'.*(\n)|($)'
+    'string': r'.*(\n)|($)',
+    'port': r'\d+',
 }
 
 FORMAT_TO_DATABASE = {
     'ip': "inet_aton('%s')",
+    'port': '%s',
     'string': "'%s'",
 }
 
@@ -50,87 +57,69 @@ def send_to_database(data_row, format, table_data_list):
     for i, el in enumerate(data_row):
         send_str += FORMAT_TO_DATABASE[format[i]] + ","
     send_str = (send_str[:-1] + ')') % tuple(data_row)
-    cursor.execute(send_str)
-    db.commit()
+    try:
+        cursor.execute(send_str)
+        db.commit()
+    except:
+        db.rollback()
+        logging.error(send_str)
 
-send_to_database([u'192.168.18.13', u'yoba2', u'some_yoba'], [u'ip', u'string', u'string'], [u'src_ip', u'str1',u'str2', u'netmap2'])
+#send_to_database([u'192.168.18.13', u'yoba2', u'some_yoba'], [u'ip', u'string', u'string'], [u'src_ip', u'str1',u'str2', u'netmap2'])
 
 def parse_table(table, format, uniq_columns):
-    data_rows = []
+    answer = []
     for i, row in enumerate(table.rows[2:]):
-        if len(row.cells) < len(format):
-            return
-        data_row = []
-        data = [0] * len(format)
-        #Нихуя не работает, блджад.
+        data = [[] for x in range(len(format))]
         for index, el in enumerate(row.cells):
-            if index >= len(format):
+            if index >= len(format) or el.text in ['',u'']:
                 break
+            #TODO Remove uniq_columns, add uniq column check
             if index in uniq_columns:
-                data[index] = re.findall(FORMAT_TO_REG_MATCH[format[index]],el.text)
+                data[index].extend(re.findall(FORMAT_TO_REG_MATCH[format[index]],el.text))
             else:
-                data[index] = [el.text]
-        t = itertools.permutations(data)
-        for l in t:
-            print(l)
+                data[index].extend([el.text])
+        data_rows = [[]]
+        for index, cell_data in enumerate(data):
+            if len(cell_data) == 1:
+                map(lambda x: x.insert(index, cell_data[0]), data_rows)
+            elif len(cell_data) > 1:
+                old_data_rows = data_rows[-1]
+                data_rows = []
+                data_rows.extend(map(lambda x: my_insert(old_data_rows, index, x), cell_data))
+        answer.extend(data_rows)
+    return [x for x in answer if x != []]
 
 
-
-def parse_docx(f):
+def parse_docx_and_send_to_db(f):
     doc = Document(f)
-    parse_table(doc.tables[1], ['ip', 'ip', 'string', 'string'], (0,1))
-
-parse_docx('/home/mikhail/Desktop/tickets/Заявка+на+предоставление+сетевой+связности %281%29_353.docx')
-
-'''
-def parse_table(table, format, uniq_tables):
-
+    format = ['ip', 'ip', 'port', 'string']
     try:
-        for i, row in enumerate(table.rows[2:]):
-            if len(row.cells) < 3:
-                continue
-            for srcip in re.findall(r'\d+\.\d+\.\d+\.\d+', row.cells[0].text):
-                for dstip in re.findall(r'\d+\.\d+\.\d+\.\d+', row.cells[1].text):
-                    for dstport in re.findall(r'\d+', row.cells[2].text):
-                        cursor.execute("INSERT INTO netmap (src_ip,dst_ip,dst_port,comment) VALUES (inet_aton(%s),inet_aton(%s),%s,%s)", (srcip,dstip,dstport,row.cells[3].text))
-                        db.commit()
+        data_rows = parse_table(doc.tables[1], format, (0,1,2))
+        logging.debug('parsed succesfully %s' % f)
     except Exception as e:
-        db.rollback()
-        raise FirstParseErrorException
+        logging.error(str(e) + '\n %s ' % f)
+        return
+    for row in data_rows:
+        send_to_database(row, format,['src_ip', 'dst_ip', 'dst_port', 'comment', 'netmap'])
 
 
-def parse_docx(f):
-    doc = Document(f)
-    try:
-        parse_first_table(doc.tables[1])
-        parse_second_table(doc.tables[2])
-    except SecondParseErrorException:
-        logging.debug('somewhat fail with second table in %s, trying 2nd variant' % f)
-        try:
-            parse_second_table_with_host_string(doc.tables[2])
-            logging.info('success %s ' % f)
-        except:
-            logging.error('epic fail with \t %s ' % f)
-    except FirstParseErrorException:
-        logging.error('Somewhat failed with first table in %s' % f)
-    except:
-        logging.error('Somewhat really failed in %s probably there are no tables there' % f)
-        print(f)
+parse_docx_and_send_to_db('/home/mikhail/Desktop/tickets/Заявка на предоставление сетевой связности Ямало Ненецкого АО_2866.docx')
+
 
 def main():
     path = sys.argv[1]
     for file_path in iterator(path):
-        print(file_path)
-        #parse_docx(file_path)
+        parse_docx_and_send_to_db(file_path)
     start_time = time.time()
     while True:
         for file_path in iterator(path):
+            pass
+            ### maybe ignore old folders?
             scan_and_send_new_tickets(start_time, file_path)
         time.sleep(600)
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.ERROR,format='%(asctime)s - %(levelname)s - %(message)s')
+    logging.basicConfig(level=logging.INFO,format='%(asctime)s - %(levelname)s - %(message)s')
     main()
 
 db.close()
-'''
