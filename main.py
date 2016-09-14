5e672a70bd814ae0bfc4c1493cd42181e05cdd96
mikhail <mandarson@gmail.com>
2016-09-14 16:30:24 +0300

    Final Polishing, changed structure a bit.

--- main.py
+++ main.py
@@ -13,22 +13,15 @@ import logging
 db = MySQLdb.connect("0.0.0.0", "root", "passw0rd", "netmap", charset="utf8", use_unicode=True, port=3306)
 cursor = db.cursor()
 
-class FirstParseErrorException(Exception):
+class ParseErrorException(Exception):
     pass
 
-class SecondParseErrorException(Exception):
-    pass
 
 def my_insert(arr, index, el):
     ans = list(arr)
     ans.insert(index, el)
     return ans
 
-def scan_and_send_new_tickets(time_float, file_path):
-    doc_creation_date = os.path.getmtime(file_path)
-    if doc_creation_date < time_float:
-        return None
-    parse_docx_and_send_to_db(file_path)
 
 def iterator(path):
     for dirpath, dirnames, files in os.walk(path):
@@ -38,7 +31,7 @@ def iterator(path):
 
 FORMAT_TO_REG_MATCH = {
     'ip': r'\d+\.\d+\.\d+\.\d+',
-    'string': r'.*(\n)|($)',
+    'string': r'.*',
     'port': r'\d+',
 }
 
@@ -50,7 +43,6 @@ FORMAT_TO_DATABASE = {
 
 def send_to_database(data_row, format, table_data_list):
     send_str = 'INSERT INTO %s ( ' % table_data_list[-1]
-    # list comprehension ???
     for i, el in enumerate(data_row):
         send_str += table_data_list[i] + ', '
     send_str = send_str[:-2] +  " ) VALUES ( "
@@ -60,28 +52,23 @@ def send_to_database(data_row, format, table_data_list):
     try:
         cursor.execute(send_str)
         db.commit()
-    except:
+    except Exception as e:
         db.rollback()
         logging.error(send_str)
-        raise FirstParseErrorException
+        raise e
 
-#send_to_database([u'192.168.18.13', u'yoba2', u'some_yoba'], [u'ip', u'string', u'string'], [u'src_ip', u'str1',u'str2', u'netmap2'])
 
-def parse_table(table, format, uniq_columns):
+def parse_table(table, format):
     answer = []
     for i, row in enumerate(table.rows[1:]):
         data = [[] for x in range(len(format))]
         for index, el in enumerate(row.cells):
             if index >= len(format) or el.text in ['',u'']:
                 break
-            #TODO Remove uniq_columns, add uniq column check
-            if index in uniq_columns:
-                found_arr = re.findall(FORMAT_TO_REG_MATCH[format[index]],el.text)
-                if found_arr == []:
-                    break
-                data[index].extend(found_arr)
-            else:
-                data[index].extend([el.text])
+            found_arr = re.findall(FORMAT_TO_REG_MATCH[format[index]],el.text)
+            if found_arr == []:
+                break
+            data[index].extend(found_arr)
         data_rows = [[]]
         for index, cell_data in enumerate(data):
             if len(cell_data) == 1:
@@ -93,38 +80,44 @@ def parse_table(table, format, uniq_columns):
         answer.extend(data_rows)
     return [x for x in answer if x != []]
 
-
-def parse_docx_and_send_to_db(f):
+def parse_docx_and_send_to_db(f, format, table_number):
     try:
         doc = Document(f)
-        format = ['ip', 'ip', 'port', 'string']
-        data_rows = parse_table(doc.tables[1], format, (0,1,2))
+        data_rows = parse_table(doc.tables[table_number], format)
         logging.debug('parsed succesfully %s' % f)
     except Exception as e:
         logging.error(str(e) + '\n %s ' % f)
-        return
+        raise ParseErrorException
     for row in data_rows:
         if len(row) < len(format):
             break
         try:
             send_to_database(row, format,['src_ip', 'dst_ip', 'dst_port', 'comment', 'netmap'])
         except:
+            raise ParseErrorException
+
+#TODO переписать через декораторы
+def scan_and_send_new_tickets(time_float, path, format, table_number):
+    success_count = 0
+    for index, file_path in enumerate(iterator(path)):
+        doc_creation_date = os.path.getmtime(file_path)
+        if doc_creation_date < time_float:
+            continue
+        try:
+            parse_docx_and_send_to_db(file_path, format, table_number)
+            success_count += 1
+        except ParseErrorException:
             pass
-
-
-parse_docx_and_send_to_db('/home/mikhail/Desktop/tickets/Заявка+на+предоставление+сетевой+связности Patrol_415.docx')
+    logging.info('ALL %d \nSUCCESSFULL %d', index, success_count)
 
 
 def main():
     path = sys.argv[1]
-    for file_path in iterator(path):
-        parse_docx_and_send_to_db(file_path)
-    start_time = time.time()
+    format = ['ip', 'ip', 'port', 'string']
+    scan_and_send_new_tickets(0,  path, format, table_number=1)
     while True:
-        for file_path in iterator(path):
-            pass
-            ### maybe  just ignore old folders?
-            scan_and_send_new_tickets(start_time, file_path)
+        start_time = time.time()
+        scan_and_send_new_tickets(start_time, path, format, table_number=1)
         time.sleep(600)
 
 if __name__ == '__main__':
