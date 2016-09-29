c99da678168e03f412399cf2d1ef05d18e4ac0a5
mikhail <mandarson@gmail.com>
2016-09-29 19:47:05 +0300

    derp, helper method rename

--- main_with_es.py
+++ main_with_es.py
@@ -17,6 +17,7 @@ def my_insert(arr, index, el):
     ans.insert(index, el)
     return ans
 
+networksConstants.main()
 es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 
 def iterator(path):
@@ -40,7 +41,7 @@ def parse_table(table, format):
     for i, row in enumerate(table.rows[1:]):
         data = [[] for x in range(len(format))]
         for index, el in enumerate(row.cells):
-            if index >= len(format) or el.text in ['',u'']:
+            if index >= len(format) or (el.text in ['', u''] and format[index] != 'string'):
                 break
             found_arr = re.findall(FORMAT_TO_REG_MATCH[format[index]],el.text)
             if found_arr == []:
@@ -53,6 +54,7 @@ def parse_table(table, format):
             elif len(cell_data) > 1:
                 old_data_rows = data_rows[-1]
                 data_rows = []
+                #TODO Тут бага
                 data_rows.extend(map(lambda x: my_insert(old_data_rows, index, x), cell_data))
         answer.extend(data_rows)
     return [x for x in answer if len(x) == len(format)]
@@ -60,10 +62,17 @@ def parse_table(table, format):
 
 def scan_and_send_new_tickets(time_float, path):
     success_count = 0
+    all = 0
     for index, file_path in enumerate(iterator(path)):
+        last_folder = file_path.split('/')[-2]
+        ticket_type = last_folder[:3]
+        ticket_number = last_folder[3:]
+        if ticket_type not in ('INC'):
+            continue
         doc_creation_date = os.path.getmtime(file_path)
         if doc_creation_date < time_float:
             continue
+        all +=1
         try:
             doc = Document(file_path)
             ticket_name = file_path.split('/')[-2]
@@ -76,8 +85,8 @@ def scan_and_send_new_tickets(time_float, path):
             my_dicts = []
             [my_dicts.append(dict([tpl for tpl in zip(FORMAT_NAMES, x)])) for x in data]
             [el.update([('dateAdded', time_to_add)]) for el in my_dicts]
-
             b = unicode(json.dumps(my_dicts[:-1]))
+            ports_desc = {22:'ssh', 20:'ftp',21:'ftp',1433:'sql',3389:'RDP'}
             for el in my_dicts:
                 try:
                     src_ip = netaddr.IPAddress(el['ip_host'])
@@ -85,18 +94,27 @@ def scan_and_send_new_tickets(time_float, path):
                 except:
                     continue
                 el['dst_network'] = 'UNKNOWN'
+                el['port_desc'] = 'UNKNOWN PORT'
+                el['ticket_type'] = ticket_type
+                el['ticket_number'] = ticket_number
+                port = int(el['port_dest'])
+                el['ip_port_triple'] = ':'.join(list(map(str, [src_ip, dst_ip, port])))
+                print(el['ip_port_triple'])
+                if port in ports_desc.keys():
+                    el['port_desc'] = ports_desc[port]
+
                 #TODO ПЕРЕПИСАТЬ НА СЕТЫ СРОЧНО
-                for cod_name, cod in networksConstants.NETWORKS_DICT.iteritems():
+                for cod_name, cod in networksConstants.MAIN_DICT.iteritems():
                     for network in cod:
                         if dst_ip in network:
                             el['dst_network'] = cod_name
                 es.index(index='tickets', doc_type='first_table',body=unicode(json.dumps(el)))
         except Exception as e:
             pass
-    logging.info('ALL %d \nSUCCESSFULL %d', index, success_count)
+    logging.info('ALL %d \nSUCCESSFULL %d', all, success_count)
 
 def main():
-    path = '/home/mikhail'
+    path = '/home/mikhail/Desktop/tickets/'
     create_mapping(es, 'tickets')
     scan_and_send_new_tickets(0,  path)
     while True:
@@ -106,6 +124,6 @@ def main():
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO,format='%(asctime)s - %(levelname)s - %(message)s')
-    es.indices.delete(index='tickets')
+    #es.indices.delete(index='tickets')
     main()
 
